8f2504d02ca648c9c8bfb339ada2183918938c77
namnh <namnh663@gmail.com>
2024-11-26 02:36:28 +0700

    chore: Update Supabase configuration to load environment variables and improve artifact upload in CI workflow

--- admin/flutter_application/lib/services/supabase_config.dart
+++ admin/flutter_application/lib/services/supabase_config.dart
@@ -2,14 +2,27 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SupabaseConfig {
-  static final supabase = Supabase.instance.client;
+  static late SupabaseClient supabase;
 
   static Future<void> initialize() async {
+    // Load environment variables from the .env.local file
     await dotenv.load(fileName: '.env');
 
+    // Retrieve values from environment variables
+    final supabaseUrl = dotenv.env['REACT_APP_SUPABASE_URL'];
+    final supabaseKey = dotenv.env['REACT_APP_SUPABASE_KEY'];
+
+    if (supabaseUrl == null || supabaseKey == null) {
+      throw Exception('Supabase URL or Key is not set in the .env file');
+    }
+
+    // Initialize Supabase with the retrieved URL and key
     await Supabase.initialize(
-      url: 'REACT_APP_SUPABASE_URL',
-      anonKey: 'REACT_APP_SUPABASE_KEY',
+      url: supabaseUrl,
+      anonKey: supabaseKey,
     );
+
+    // Assign the initialized client
+    supabase = Supabase.instance.client;
   }
 }
